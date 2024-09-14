8c54f1564be735d26f3bb3140315988432247d1b
adarSonmez <adarsonmez@outlook.com>
2024-09-14 14:04:55 +0300

    Add RelayCommand and BaseViewModel

---
+++ Commands/RelayCommand.cs
@@ -0,0 +1,39 @@
+﻿using System.Windows.Input;
+
+namespace ChatCat.Desktop.Commands
+{
+    /// <summary>
+    /// Represents a command that relays its functionality to delegates.
+    /// </summary>
+    internal class RelayCommand : ICommand
+    {
+        private readonly Predicate<object?>? canExecute;
+        private readonly Action<object?> execute;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="RelayCommand"/> class.
+        /// </summary>
+        /// <param name="execute">The delegate to execute when the command is executed.</param>
+        /// <param name="canExecute">The delegate to determine if the command can be executed.</param>
+        public RelayCommand(Action<object?> execute, Predicate<object?>? canExecute = null)
+        {
+            this.execute = execute ?? throw new ArgumentNullException(nameof(execute));
+            this.canExecute = canExecute;
+        }
+
+        /// <inheritdoc/>
+        public event EventHandler? CanExecuteChanged;
+
+        /// <inheritdoc/>
+        public bool CanExecute(object? parameter)
+        {
+            return canExecute == null || canExecute(parameter);
+        }
+
+        /// <inheritdoc/>
+        public void Execute(object? parameter)
+        {
+            execute(parameter);
+        }
+    }
+}
---
+++ ViewModels/Base/BaseViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel;
+
+namespace ChatCat.Desktop.ViewModels.Base
+{
+    /// <summary>
+    /// Base view model class that implements the INotifyPropertyChanged interface.
+    /// </summary>
+    internal class BaseViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        /// <summary>
+        /// Raises the PropertyChanged event for the specified property name.
+        /// </summary>
+        /// <param name="propertyName">The name of the property that changed.</param>
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
