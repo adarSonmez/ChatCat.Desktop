667d7e0889e8a49985b48600f374de0dff2a7246
adarSonmez <adarsonmez@outlook.com>
2024-09-14 21:31:49 +0300

    Complete main window

--- ViewModels/MainWindowVM.cs
+++ ViewModels/MainWindowVM.cs
@@ -1,5 +1,7 @@
-﻿using ChatCat.Desktop.ViewModels.Base;
+﻿using ChatCat.Desktop.Commands;
+using ChatCat.Desktop.ViewModels.Base;
 using System.Windows;
+using System.Windows.Input;
 
 namespace ChatCat.Desktop.ViewModels
 {
@@ -9,7 +11,7 @@ namespace ChatCat.Desktop.ViewModels
 
         private Window _mainWindow;
         private int _outerMarginSize = 10;
-        private int _windowRadiusSize = 16;
+        private int _windowRadiusSize = 12;
 
         #endregion Private Fields
 
@@ -28,8 +30,9 @@ namespace ChatCat.Desktop.ViewModels
         public Thickness BorderRadiusThickness => new(ResizeBorderSize + OuterMarginSize);
         public Thickness OuterMarginSizeThickness => new(OuterMarginSize);
         public CornerRadius WindowCornerRadius => new(WindowRadiusSize);
+        public GridLength TitleHeightGridLength => new(TitleBarHeight + ResizeBorderSize);
 
-        public int TitleBarHeight { get; set; } = 42;
+        public int TitleBarHeight { get; set; } = 32;
         public int ResizeBorderSize { get; set; } = 12;
 
         public int WindowRadiusSize
@@ -45,6 +48,15 @@ namespace ChatCat.Desktop.ViewModels
         }
 
         #endregion Public Properties
+
+        #region Commands
+
+        public ICommand MinimizeCommand => new RelayCommand((_) => _mainWindow.WindowState = WindowState.Minimized);
+        public ICommand MaximizeCommand => new RelayCommand((_) => _mainWindow.WindowState ^= WindowState.Maximized);
+        public ICommand CloseCommand => new RelayCommand((_) => _mainWindow.Close());
+        public ICommand MenuCommand => new RelayCommand((_) => SystemCommands.ShowSystemMenu(_mainWindow, GetMousePosition()));
+
+        #endregion Commands
 
         #region Constructors
 
@@ -63,5 +75,15 @@ namespace ChatCat.Desktop.ViewModels
         }
 
         #endregion Constructors
+
+        #region Private Methods
+
+        private Point GetMousePosition()
+        {
+            var position = Mouse.GetPosition(_mainWindow);
+            return new Point(position.X + _mainWindow.Left, position.Y + _mainWindow.Top);
+        }
+
+        #endregion Private Methods
     }
 }
