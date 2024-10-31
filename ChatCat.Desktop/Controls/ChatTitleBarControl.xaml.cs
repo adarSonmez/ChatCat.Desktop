59a672bc8c3c3b7ac0bafaefeee19bb08ef1035e
adarSonmez <adarsonmez@outlook.com>
2024-10-31 23:29:39 +0300

    Add ChatTitleBarControl.xaml

---
+++ ChatCat.Desktop/Controls/ChatTitleBarControl.xaml.cs
@@ -0,0 +1,15 @@
+﻿using ChatCat.Desktop.Controls.Base;
+
+namespace ChatCat.Desktop.Controls
+{
+    /// <summary>
+    /// Interaction logic for ChatTitleBarControl.xaml
+    /// </summary>
+    public partial class ChatTitleBarControl : BaseControl
+    {
+        public ChatTitleBarControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
