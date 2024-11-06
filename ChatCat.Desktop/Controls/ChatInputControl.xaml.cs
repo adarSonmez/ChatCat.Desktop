97be4c71c5d975a066f2b7919430cb7aca07521f
adarSonmez <adarsonmez@outlook.com>
2024-11-07 00:25:10 +0300

    Create expandable chat message input control

---
+++ ChatCat.Desktop/Controls/ChatInputControl.xaml.cs
@@ -0,0 +1,15 @@
+﻿using ChatCat.Desktop.Controls.Base;
+
+namespace ChatCat.Desktop.Controls
+{
+    /// <summary>
+    /// Interaction logic for ChatInputControl.xaml
+    /// </summary>
+    public partial class ChatInputControl : BaseControl
+    {
+        public ChatInputControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
